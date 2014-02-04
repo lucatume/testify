0e96bfe41d0560482c3dc5927365cdb54c848195
Luca Tumedei <luca@theaveragedev.com>
2014-02-04 19:07:34 +0100

    initial commit

---
+++ HumanTestNames.py
@@ -0,0 +1,25 @@
+import sublime, sublime_plugin ,re
+
+class HumanTestNamesCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for region in self.view.sel():
+            if not region.empty():
+                    # get the selected text
+                    s = self.view.substr(region)
+                    # split the text in lines
+                    lines = s.splitlines()
+                    newLines = 'newLines'
+                    for line in lines:
+                        # transform to uppercase
+                        s = line.title()
+                        # remove spaces
+                        s = re.sub("\\s+", "", s)
+                        # prepend text
+                        s = 'public function test' + s
+                        # append text
+                        plcHolder = "$this->markTestIncomplete(\n'This test has not been implemented yet.'\n');"
+                        s = s + "()\n{\n" + plcHolder + "\n}\n\n"
+                        # append modified line
+                        newLines = newLines + s
+                    # replace in text
+                    self.view.replace(edit, region, newLines)
