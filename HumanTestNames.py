8f3c990fc13af2dd4819a106f46cf5f4e2667b8e
Luca Tumedei <luca@theaveragedev.com>
2014-02-07 11:36:27 +0100

    moved some pieces around to make it all more oop

--- HumanTestNames.py
+++ HumanTestNames.py
@@ -1,8 +1,12 @@
-import sublime, sublime_plugin , re
+import sublime
+import sublime_plugin
+import re
+
 
 class HumanTestNamesCommand(sublime_plugin.TextCommand):
     variableName = ''
     blockText = ''
+
     def run(self, edit):
         for region in self.view.sel():
             if not region.empty():
@@ -12,42 +16,86 @@ class HumanTestNamesCommand(sublime_plugin.TextCommand):
                     lines = s.splitlines()
                     newLines = ''
                     for line in lines:
-                        # set block text if any
-                        if self.containsDataProviderToken(line):
-                            self. setDataProviderMethodTextAndCommentBlockUsing(self.getDataProviderTokenTextFrom(line))
-                        # transform to uppercase
-                        s = line.title()
-                        # remove spaces
-                        s = re.sub("\\s+", "", s)
-                        # prepend text
-                        s = self.blockText + 'public function test' + s
-                        # append text
-                        plcHolder ="\t$this->markTestIncomplete('This test has not been implemented yet.');"
-                        s = s + "(%s)\n{\n" % (self.variableName)
-                        s = s + plcHolder + "\n}\n"
+                        out = ''
+                        pre = ''
+                        testMethodName = ''
+                        testMethodBody = ''
+                        post = ''
+                        # get new generator instances
+                        dp = DataProviderGenerator(line)
+                        variableName = ''
+                        # check for the data provider token
+                        if dp.containsDataProviderToken():
+                            pre += dp.getDataProviderMethodTextAndCommentBlock()
+                            variableName = dp.getVariableName()
+                        tg = TestMethodGenerator(line, variableName)
+                        testMethodName = tg.getTestMethodName()
+                        testMethodBody = tg.getTestMethodBody()
+                        out += pre
+                        out += testMethodName
+                        out += testMethodBody
+                        out += post
                         # append modified line
-                        newLines = newLines + s
+                        newLines = newLines + out
                     # replace in text
                     self.view.replace(edit, region, newLines)
                     # reindent output
                     self.view.run_command('reindent')
-    def containsDataProviderToken(self, searchText):
-        if re.search("@dp\\s+", searchText):
-            return True
-        return False
-    def getDataProviderTokenTextFrom(self, searchText):
-        result = re.sub("^.*@dp\\s([\\w\\s]*)@*.*", "\\1", searchText)
-        return result
-    def setDataProviderMethodTextAndCommentBlockUsing(self, text):
-        # remove leading and trailing spaces
-        text = text.strip()
-        # create the dataProviderMethodName
-        methodName = text.title()
-        methodName = re.sub("\\s+", "", methodName)
-        self.variableName = '$' + methodName[0].lower() + methodName[1:]
-        methodName =  methodName + 'Provider'
-        methodName = methodName[0].lower() + methodName[1:]
-        # create data provider method placeholder text like
-        commentText = text
-        # replace the strings in the template
-        self.blockText = "public function %s(){\nreturn array(\n\t\t\t//%s\n);\n}\n\t/**\n\t * @dataProvider %s\n\t */\n" % (methodName, commentText, methodName)
+
+
+class TestMethodGenerator:
+
+    def __init__(self, text, variableName=''):
+        self.variableName = variableName
+        if variableName != '':
+            self.variableName = '$'+variableName
+        self.text = self.removeGeneratorTokensFrom(text)
+
+    def removeGeneratorTokensFrom(self, text):
+        return re.sub("#", "", text)
+
+    def getTestMethodName(self):
+        # make a title
+        out = self.text.title().strip()
+        # remove spaces
+        out = re.sub("\\s+", "", out)
+        out = 'public function test' + out
+        out += '(' + self.variableName + ')\n'
+        return out
+
+    def getTestMethodBody(self):
+        out = ''
+        out += '{'
+        out += "\n\t$this->markTestIncomplete('This test has not been implemented yet.');"
+        out += '\n}\n'
+        return out
+
+class DataProviderGenerator:
+
+    def __init__(self, text):
+        self.text = text
+
+    def containsDataProviderToken(self):
+        # search for the opening token
+        return re.search("\\s+#", self.text)
+
+    def getVariableName(self):
+        out = re.sub("(.*)\\s+#([^#]*)\\s*(.*)", "\\2", self.text)
+        out = out.strip().title()
+        out = out[0].lower() + out[1:]
+        out = re.sub('\\s', '', out)
+        return out
+
+    def getDataProviderMethodTextAndCommentBlock(self):
+        dataProviderMethodName = self.getVariableName() + 'Provider()'
+        out = ''
+        out += '\npublic function ' + dataProviderMethodName
+        out += '\n{'
+        out += '\n\treturn array('
+        out += '\n\t\t// ' + self.getVariableName()
+        out += '\n\t);'
+        out += '\n}'
+        out += '\n/**'
+        out += '\n * @dataProvider ' + dataProviderMethodName
+        out += '\n*/'
+        return out
